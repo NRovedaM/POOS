71c584932de6c60f6b2190a4abb135009a8901a8
NRovedaM <nicolasrovedamelo@gmail.com>
2025-03-31 12:37:58 -0400

    Add files via upload

---
+++ lista de exercicios de sabado/ex_02.py
@@ -0,0 +1,5 @@
+#Crie um programa que peça ao usuário para digitar seu nome e idade, e exiba: "Olá, {nome}! Você tem {idade} anos."
+nome = input("digite seu nome: ")
+idade= int(input("digite sua idade: "))
+
+print(f"Olá {nome}! Você tem {idade} anos.")
